865ed0c9e0214f0a31c425ccb289743827c5989f
Mehedi_Hassan <mehedi.github@gmail.com>
2025-02-21 19:31:16 +0600

    updated

--- app/src/main/java/com/example/navigationjetpackcompose/Screen.kt
+++ app/src/main/java/com/example/navigationjetpackcompose/Screen.kt
@@ -6,7 +6,8 @@ sealed class Screen(val route: String) {
     data object Home : Screen(route = "home_screen")
     data object Details : Screen(route = "details_screen/{$DETAIL_ARGUMENT_KEY}") {
         fun passId(id: Int): String {
-            return "details_screen/$id"
+            //return "details_screen/$id"
+            return this.route.replace(oldValue = "{$DETAIL_ARGUMENT_KEY}", newValue = id.toString())
         }
     }
 }
